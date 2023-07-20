7dcc3aac1daf36ede87771871965e3aba0866b68
SpasPandev <sppan@abv.bg>
2023-07-20 21:34:46 +0300

    User etity created

---
+++ security/src/main/java/com/example/security/user/User.java
@@ -0,0 +1,23 @@
+package com.example.security.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
