84bf30d7feb75322365a7ca16ebb4f643caafc92
SpasPandev <sppan@abv.bg>
2023-07-21 22:14:26 +0300

    Check if the token is valid

--- security/src/main/java/com/example/security/cofig/JwtService.java
+++ security/src/main/java/com/example/security/cofig/JwtService.java
@@ -47,6 +47,23 @@ public class JwtService {
                 .compact();
     }
 
+    private boolean isTokenValid(String token, UserDetails userDetails) {
+
+        final String username = extractUsername(token);
+
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     private Claims extractAllClaims(String token) {
 
         return Jwts
