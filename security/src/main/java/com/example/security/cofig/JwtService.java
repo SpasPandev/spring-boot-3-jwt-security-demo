3e7c15b0cfa424aedf0adab01043ae30fad229f6
SpasPandev <sppan@abv.bg>
2023-07-21 22:45:43 +0300

    Update the SecurityContextHolder and finalise the filter

--- security/src/main/java/com/example/security/cofig/JwtService.java
+++ security/src/main/java/com/example/security/cofig/JwtService.java
@@ -47,7 +47,7 @@ public class JwtService {
                 .compact();
     }
 
-    private boolean isTokenValid(String token, UserDetails userDetails) {
+    public boolean isTokenValid(String token, UserDetails userDetails) {
 
         final String username = extractUsername(token);
 
