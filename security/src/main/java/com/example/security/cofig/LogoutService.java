56a47acd302b2b06df46e0c4a7a5c1b0376f0b78
SpasPandev <sppan@abv.bg>
2023-07-27 18:10:39 +0300

    Create the Logout handler service

---
+++ security/src/main/java/com/example/security/cofig/LogoutService.java
@@ -0,0 +1,18 @@
+package com.example.security.cofig;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService implements LogoutHandler {
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+
+    }
+}
--- security/src/main/java/com/example/security/cofig/SecurityConfiguration.java
+++ security/src/main/java/com/example/security/cofig/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +20,7 @@ public class SecurityConfiguration {
 
     private final AuthenticationProvider authenticationProvider;
     private final JwtAuthenticationFilter jwtAuthFilter;
+    private final LogoutHandler logoutHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -39,7 +41,7 @@ public class SecurityConfiguration {
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .logout()
                 .logoutUrl("/api/v1/auth/logout")
-                .addLogoutHandler(null)
+                .addLogoutHandler(logoutHandler)
                 .logoutSuccessHandler((request, response, authentication) ->
                         SecurityContextHolder.clearContext());
 
