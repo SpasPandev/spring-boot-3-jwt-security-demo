c8072cad95f9f229e98e38c5986249a23fbf67f1
SpasPandev <sppan@abv.bg>
2023-07-27 18:24:06 +0300

    Implement the logout handler service

--- security/src/main/java/com/example/security/cofig/LogoutService.java
+++ security/src/main/java/com/example/security/cofig/LogoutService.java
@@ -1,5 +1,6 @@
 package com.example.security.cofig;
 
+import com.example.security.token.TokenRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +12,27 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LogoutService implements LogoutHandler {
 
+    private final TokenRepository tokenRepository;
+
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
 
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+
+        var storedToken = tokenRepository.findByToken(jwt).orElse(null);
+
+        if (storedToken != null) {
+            storedToken.setExpired(true);
+            storedToken.setRevoked(true);
+
+            tokenRepository.save(storedToken);
+        }
     }
 }
