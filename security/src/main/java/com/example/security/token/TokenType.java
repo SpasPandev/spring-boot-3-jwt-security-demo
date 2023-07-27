b7faed3dca174cd14b6a8a136af61985bd2490ed
SpasPandev <sppan@abv.bg>
2023-07-27 11:45:15 +0300

    Create the Token entity

---
+++ security/src/main/java/com/example/security/token/TokenType.java
@@ -0,0 +1,6 @@
+package com.example.security.token;
+
+public enum TokenType {
+
+    BEARER
+}
--- security/src/main/java/com/example/security/user/User.java
+++ security/src/main/java/com/example/security/user/User.java
@@ -1,5 +1,6 @@
 package com.example.security.user;
 
+import com.example.security.token.Token;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,6 +29,8 @@ public class User implements UserDetails {
     private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
+    @OneToMany(mappedBy = "user")
+    private List<Token> tokens;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
