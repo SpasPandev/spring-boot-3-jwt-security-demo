6176da715797ee46cbb7d98ab4f1e982696ace4a
SpasPandev <sppan@abv.bg>
2023-07-27 18:07:07 +0300

    Add the security configuration

--- security/src/main/java/com/example/security/cofig/SecurityConfiguration.java
+++ security/src/main/java/com/example/security/cofig/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -35,7 +36,12 @@ public class SecurityConfiguration {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout()
+                .logoutUrl("/api/v1/auth/logout")
+                .addLogoutHandler(null)
+                .logoutSuccessHandler((request, response, authentication) ->
+                        SecurityContextHolder.clearContext());
 
         return http.build();
     }
